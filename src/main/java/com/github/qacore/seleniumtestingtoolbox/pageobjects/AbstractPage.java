5ade9d3db9487df717d2b82483ca79372bf4a75c
Leonardo Carmona <leocarmona_@outlook.com>
2017-08-10 15:26:22 -0300

    Inclusion of Page Object pattern

---
+++ src/main/java/com/github/qacore/seleniumtestingtoolbox/pageobjects/AbstractPage.java
@@ -0,0 +1,99 @@
+package com.github.qacore.seleniumtestingtoolbox.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.internal.WrapsDriver;
+
+import com.github.qacore.seleniumtestingtoolbox.WebDriverContext;
+
+/**
+ * 
+ * 
+ * @author Leonardo Carmona da Silva
+ *         <ul>
+ *         <li><a href="https://br.linkedin.com/in/l3ocarmona">https://br.linkedin.com/in/l3ocarmona</a></li>
+ *         <li><a href="https://github.com/leocarmona">https://github.com/leocarmona</a></li>
+ *         <li><a href="mailto:lcdesenv@gmail.com">lcdesenv@gmail.com</a></li>
+ *         </ul>
+ *
+ * @param <I>
+ *            Itself class for fluent pattern.
+ *
+ * @since 1.0.0
+ * 
+ * @see WebDriverContext
+ *
+ */
+public abstract class AbstractPage<I extends AbstractPage<I>> extends WebDriverContext {
+
+    public AbstractPage(WrapsDriver driverContext) {
+        super(driverContext);
+    }
+
+    public AbstractPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public AbstractPage() {
+        super();
+    }
+
+    /**
+     * Load the page in the current browser window.
+     * 
+     * @param path
+     *            The URL path after {@code getUrl()}.
+     * 
+     * @return Itself.
+     */
+    public I load(String path) {
+        this.getWrappedDriver().get(this.getUrl() + path);
+
+        return itself();
+    }
+
+    /**
+     * Load the page in the current browser window.
+     * 
+     * @return Itself.
+     */
+    public I load() {
+        return this.load("");
+    }
+
+    /**
+     * Determine whether or not the page is loaded. When the page is loaded, this method will return, but when it is not loaded, an Error should be thrown. This also allows for complex checking and error reporting when loading a page, which in turn supports better error reporting when a page fails to load.
+     *
+     * <p>
+     * This behaviour makes it readily visible when a page has not been loaded successfully, and because an error and not an exception is thrown tests should fail as expected. By using Error, we also allow the use of junit's "Assert.assert*" methods.
+     * </p>
+     *
+     * @return Itself.
+     * 
+     * @throws Error
+     *             When the page is not loaded.
+     */
+    public abstract I isLoaded() throws Error;
+
+    /**
+     * The URL to load. It is best to use a fully qualified URL.
+     * 
+     * @return The URL to load.
+     */
+    public abstract String getUrl();
+
+    /**
+     * The generic parameter.
+     * 
+     * @return Itself.
+     */
+    @SuppressWarnings("unchecked")
+    protected I itself() {
+        return (I) this;
+    }
+    
+    @Override
+    public String toString() {
+        return "Page(" + this.getUrl() + ")";
+    }
+
+}
---
+++ src/main/java/com/github/qacore/seleniumtestingtoolbox/pageobjects/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides classes to support Page Object pattern.
+ */
+package com.github.qacore.seleniumtestingtoolbox.pageobjects;
