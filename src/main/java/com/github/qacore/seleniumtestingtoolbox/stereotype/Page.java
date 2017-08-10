80ac6889722aacbd9fe4521487b7805cd13c06b6
Leonardo Carmona <leocarmona_@outlook.com>
2017-08-10 12:06:45 -0300

    Inclusion of Page and PageComponent

---
+++ src/main/java/com/github/qacore/seleniumtestingtoolbox/stereotype/Page.java
@@ -0,0 +1,29 @@
+package com.github.qacore.seleniumtestingtoolbox.stereotype;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Mark a field as a Page Object that should be automatically created by Selenium Testing Toolbox.
+ * 
+ * @author Leonardo Carmona da Silva
+ *         <ul>
+ *         <li><a href="https://br.linkedin.com/in/l3ocarmona">https://br.linkedin.com/in/l3ocarmona</a></li>
+ *         <li><a href="https://github.com/leocarmona">https://github.com/leocarmona</a></li>
+ *         <li><a href="mailto:lcdesenv@gmail.com">lcdesenv@gmail.com</a></li>
+ *         </ul>
+ *
+ * @since 1.0.0
+ *
+ */
+@Target({ TYPE, FIELD })
+@Retention(RUNTIME)
+@Documented
+public @interface Page {
+
+}
